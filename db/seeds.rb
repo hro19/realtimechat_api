cb6509fb149081396046413aa48f22e96da9defb
hro19 <fourbasic19@gmail.com>
2023-12-25 21:46:35 +0900

    seedsを作成（room,message）

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Room.create(name: '週末の予定')
+Room.create(name: 'ランチ会')
+
+Message.create(content: 'Hello!', sender_name: '太郎', room: Room.first)
+Message.create(content: 'Hi!', sender_name: '又吉', room: Room.first)
+Message.create(content: 'How are you?', sender_name: '太郎', room: Room.first)
+Message.create(content: 'Fine, thanks!', sender_name: '梅子', room: Room.last)
