eafc486678dc991e05dd839cf88fde8ded9a10e5
hro19 <fourbasic19@gmail.com>
2023-12-25 16:56:38 +0900

    rack-corsによる指定のポートだけのリクエスト許可

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
-class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::API
+  # 以下を追加
+  def test
+    render json: { message: 'これはrealtimechat_apiからのrails7経由のメッセです。' }
+  end
 end
