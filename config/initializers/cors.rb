e9ceda9f2fd4510f94ba0b0bbfe8da20c6417fc4
hro19 <fourbasic19@gmail.com>
2023-12-28 19:34:46 +0900

    corsのURLを追記

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:5173' # Vue.jsを動作させているアドレスとポート番号
+    origins 'localhost:5173', 'https://realtimechat-vue.vercel.app' # Vue.jsを動作させているアドレスとポート番号、および追加したいオリジン
 
     resource '*',
       headers: :any,
